a44115802f32e04550da97bcb3d4849408750f4f
Syed Subtain Haider <sibtainhaider369@gmail.com>
2024-12-17 18:40:18 +0400

    publiclink

--- server.js
+++ server.js
@@ -33,15 +33,6 @@ app.use(notFound);
 // Error Handling
 app.use(errorHandler);
 
-// Serve static files (uploaded files) from the 'uploads' directory
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
-app.get("/api/test-video", (req, res) => {
-    const filePath = path.join(__dirname, "uploads", "1734438526087-220307751-videos_3.mp4");
-    console.log("Serving file:", filePath); // Debugging line
-    res.sendFile(filePath);
-});
-
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
